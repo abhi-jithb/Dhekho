556d4dc1392641d94897b25b5b1c4d38199ae44c
Abhijith B <babhijith58@gmail.com>
2026-08-01 09:46:01 +0530

    add: first model

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,24 @@
+import { timeStamp } from 'console';
 import * as vscode from 'vscode';
+import {Activity} from "./models/Activity"
 
 export function activate(context : vscode.ExtensionContext){
     console.log('First line of hard code in building VS Code Extension');
 
     context.subscriptions.push(
-        vscode.window.onDidChangeActiveTextEditor(()=>{
+        vscode.window.onDidChangeActiveTextEditor((edi)=>{
             console.log("Active editor changed!");
+
+            if(edi){
+                const activity : Activity = {
+                    type: "Active File Changed!",
+                    file: edi.document.fileName,
+                    timeStamp: new Data()
+                }
+                console.log(activity);
+            } else{
+                console.log("no active editor!");
+            }
         })
     );
 }
