514ed9854a1474d3ffed616edf6d16f8bd061c76
Abhijith B <babhijith58@gmail.com>
2026-08-07 23:11:05 +0530

    server createdwith new architecture

---
+++ dhekho-extension/src/extension.ts
@@ -0,0 +1,23 @@
+import * as vscode from 'vscode';
+import { createActivity } from './services/ActivityService';
+import { publish } from './services/ActivityPublisher';
+
+export function activate(context : vscode.ExtensionContext){
+    console.log('First line of hard code in building VS Code Extension');
+
+    context.subscriptions.push(
+        vscode.window.onDidChangeActiveTextEditor((edi)=>{
+            if(edi){
+                const activity = createActivity(
+                    "Active File Changed!",
+                    edi.document.fileName
+                );
+                publish(activity)
+            } else{
+                console.log("no active editor!");
+            }
+        })
+    );
+}
+
+export function deactivate(){}
